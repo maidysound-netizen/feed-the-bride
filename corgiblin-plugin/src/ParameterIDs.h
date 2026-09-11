ea574d767bd6a41182c85756efdcb9076c79570e
maidysound-netizen <maidysound@gmail.com>
2026-09-11 16:33:18 +0900

    Add CORGIBLIN parameter IDs

---
+++ corgiblin-plugin/src/ParameterIDs.h
@@ -0,0 +1,19 @@
+#pragma once
+namespace ParamIDs
+{
+    inline constexpr auto waveform  = "waveform";
+    inline constexpr auto attack    = "attack";
+    inline constexpr auto decay     = "decay";
+    inline constexpr auto sustain   = "sustain";
+    inline constexpr auto release   = "release";
+    inline constexpr auto cutoff    = "cutoff";
+    inline constexpr auto resonance = "resonance";
+    inline constexpr auto master    = "master";
+    inline constexpr auto heckLevel = "heckLevel";
+    inline constexpr auto heckTune  = "heckTune";
+    inline constexpr auto drive     = "drive";
+    inline constexpr auto delayMix  = "delayMix";
+    inline constexpr auto delayTime = "delayTime";
+    inline constexpr auto feedback  = "feedback";
+    inline constexpr auto reverbMix = "reverbMix";
+}
