fd57e84d7b406b5bac981337c0dfb8eb0902f86b
maidysound-netizen <maidysound@gmail.com>
2026-09-11 16:34:40 +0900

    Add CORGIBLIN plugin processor header

---
+++ corgiblin-plugin/src/PluginProcessor.h
@@ -0,0 +1,42 @@
+#pragma once
+#include <JuceHeader.h>
+
+class CorgiblinRecorderAudioProcessor : public juce::AudioProcessor
+{
+public:
+    CorgiblinRecorderAudioProcessor();
+    ~CorgiblinRecorderAudioProcessor() override = default;
+
+    void prepareToPlay (double sampleRate, int samplesPerBlock) override;
+    void releaseResources() override {}
+    bool isBusesLayoutSupported (const BusesLayout& layouts) const override;
+    void processBlock (juce::AudioBuffer<float>&, juce::MidiBuffer&) override;
+
+    juce::AudioProcessorEditor* createEditor() override;
+    bool hasEditor() const override { return true; }
+    const juce::String getName() const override { return JucePlugin_Name; }
+    bool acceptsMidi() const override { return true; }
+    bool producesMidi() const override { return false; }
+    bool isMidiEffect() const override { return false; }
+    double getTailLengthSeconds() const override { return 2.0; }
+
+    int getNumPrograms() override { return 1; }
+    int getCurrentProgram() override { return 0; }
+    void setCurrentProgram (int) override {}
+    const juce::String getProgramName (int) override { return {}; }
+    void changeProgramName (int, const juce::String&) override {}
+
+    void getStateInformation (juce::MemoryBlock& destData) override;
+    void setStateInformation (const void* data, int sizeInBytes) override;
+
+    juce::AudioProcessorValueTreeState apvts;
+
+private:
+    static juce::AudioProcessorValueTreeState::ParameterLayout createParameterLayout();
+
+    juce::Synthesiser synth;
+    juce::dsp::DelayLine<float> delay { 192000 };
+    juce::Reverb reverb;
+
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (CorgiblinRecorderAudioProcessor)
+};
